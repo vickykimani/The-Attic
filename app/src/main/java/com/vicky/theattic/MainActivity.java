c0b9fae3ab15231e0f6b257b81985e39bb2c7472
vickykimani <victorianightingale70@gmail.com>
2021-08-23 14:06:26 +0300

    second commit

--- app/src/main/java/com/vicky/theattic/MainActivity.java
+++ app/src/main/java/com/vicky/theattic/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.database.SnapshotParser;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -113,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            protected void onBindViewHolder(@NonNull AtticViewHolder holder, int positon, @NonNull Attic model) {
+            protected void onBindViewHolder(@NonNull AtticViewHolder holder, int position, @NonNull Attic model) {
                 final String post_key = getRef(position).getKey();
 
                 holder.setTitle(model.getTitle());
